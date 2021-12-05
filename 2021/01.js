4150fa0cd4c7ac968456e69eec9e14e8fa3826b6
s0wie <sowieh@gmail.com>
2021-12-05 23:34:53 +0100

    Did part two

--- 2021/01.js
+++ 2021/01.js
@@ -1,4 +1,5 @@
 // 1. Using node.js for the first time PART ONE
+// Got correct answer, but should be wrong since it's still a string?
 let fs = require("fs");
 let text = fs.readFileSync("2021/01.txt");
 text = text.toString();
@@ -13,3 +14,13 @@ for (i = 1; i < array.length; i++) {
 console.log(count);
 
 // 1. PART TWO
+count = 0;
+for (i = 0; i < array.length; i++) {
+  if (
+    parseInt(array[i + 1]) + parseInt(array[i + 2]) + parseInt(array[i + 3]) >
+    parseInt(array[i]) + parseInt(array[i + 1]) + parseInt(array[i + 2])
+  ) {
+    count++;
+  }
+}
+console.log(count);
