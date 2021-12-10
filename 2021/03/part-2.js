3afc7116dc28959956fc2e2110fbdc450150da84
s0wie <sowieh@gmail.com>
2021-12-10 23:50:48 +0100

    advent 3 part 2

--- 2021/03/part-2.js
+++ 2021/03/part-2.js
@@ -1,9 +1,6 @@
 const fs = require("fs");
-const array = fs.readFileSync("03.txt").toString().split("\n");
-
-let gamma = [];
-let epsilon = [];
-
+const array = fs.readFileSync("input.txt").toString().split("\n");
+// Oxygen generator rating
 for (o = 0; o < array[0].length; o++) {
   let oneCounter = 0;
   let zeroCounter = 0;
@@ -15,21 +12,62 @@ for (o = 0; o < array[0].length; o++) {
       zeroCounter++;
     }
   });
-
-  if (oneCounter >= zeroCounter) {
-    gamma.push("1");
-    epsilon.push("0");
-  } else {
-    gamma.push("0");
-    epsilon.push("1");
+  for (p = 0; p < array.length; p++) {
+    if (array.length === 1) {
+      break;
+    }
+    if (oneCounter >= zeroCounter) {
+      if (array[p][o] == "0") {
+        array.splice(p, 1);
+        p--;
+      }
+    }
+    if (zeroCounter > oneCounter) {
+      if (array[p][o] == "1") {
+        array.splice(p, 1);
+        p--;
+      }
+    }
   }
 }
 
-let gammaBinary = gamma.join("");
-let epsilonBinary = epsilon.join("");
-let gammaDigit = parseInt(gammaBinary, 2);
-let epsilonDigit = parseInt(epsilonBinary, 2);
+const oxygenBinary = array[0];
+const oxygenDecimal = parseInt(oxygenBinary, 2);
+console.log(oxygenDecimal);
 
-console.log(gammaDigit);
-console.log(epsilonDigit);
-console.log(gammaDigit * epsilonDigit);
+const co2array = fs.readFileSync("input.txt").toString().split("\n");
+
+for (o = 0; o < co2array[0].length; o++) {
+  let oneCounter = 0;
+  let zeroCounter = 0;
+
+  co2array.forEach((element) => {
+    if (element[o] == 1) {
+      oneCounter++;
+    } else {
+      zeroCounter++;
+    }
+  });
+  for (p = 0; p < co2array.length; p++) {
+    if (co2array.length === 1) {
+      break;
+    }
+    if (oneCounter < zeroCounter) {
+      if (co2array[p][o] == "0") {
+        co2array.splice(p, 1);
+        p--;
+      }
+    }
+    if (zeroCounter <= oneCounter) {
+      if (co2array[p][o] == "1") {
+        co2array.splice(p, 1);
+        p--;
+      }
+    }
+  }
+}
+
+const co2Binary = co2array[0];
+const co2Decimal = parseInt(co2Binary, 2);
+console.log(co2Decimal);
+console.log(oxygenDecimal * co2Decimal);
